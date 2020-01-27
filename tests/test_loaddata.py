8e05a67409067d18c44ab5d360982b4bfbcc2538
Swarchal <scott.warchal@gmail.com>
2020-01-27 15:40:24 +0100

    rearrange testing data (towards #1)

--- tests/test_loaddata.py
+++ tests/test_loaddata.py
@@ -4,26 +4,30 @@ from cptools2 import loaddata
 from cptools2 import filelist
 
 CURRENT_PATH = os.path.dirname(__file__)
-TEST_PATH = os.path.join(CURRENT_PATH, "example_dir")
-TEST_PATH_PLATE_1 = os.path.join(TEST_PATH, "test-plate-1")
-IMG_LIST = filelist.files_from_plate(TEST_PATH_PLATE_1)
+
+################################################################################
+# imagexpress
+################################################################################
+
+TEST_PATH_IX = os.path.join(CURRENT_PATH, "example_dir_ix")
+TEST_PATH_PLATE_1_IX = os.path.join(TEST_PATH_IX, "test-plate-1")
+IMG_LIST_IX = filelist.files_from_plate(TEST_PATH_PLATE_1_IX)
 
 
-def test_create_long_loaddata():
+def test_create_long_loaddata_ix():
     """cptool2.loaddata.create_long_loaddata(img_list)"""
-    long_df = loaddata.create_long_loaddata(IMG_LIST, microscope="imagexpress")
+    long_df = loaddata.create_long_loaddata(IMG_LIST_IX, microscope="imagexpress")
     assert isinstance(long_df, pd.DataFrame)
-    assert long_df.shape[0] == len(IMG_LIST)
+    assert long_df.shape[0] == len(IMG_LIST_IX)
 
 
-def test_cast_dataframe():
+def test_cast_dataframe_ix():
     """cptools2.loaddata.cast_dataframe(dataframe)"""
-    long_df = loaddata.create_long_loaddata(IMG_LIST, microscope="imagexpress")
+    long_df = loaddata.create_long_loaddata(IMG_LIST_IX, microscope="imagexpress")
     wide_df = loaddata.cast_dataframe(long_df)
     assert isinstance(wide_df, pd.DataFrame)
     # check we have a row per imageset
     expected_rows = 60 * 6
-    wide_df.to_csv("/home/swl_sur/test.csv")
     assert wide_df.shape[0] == expected_rows
     expected_cols = sorted(["Metadata_site",
                             "Metadata_row",
@@ -44,11 +48,53 @@ def test_cast_dataframe():
     assert sorted(wide_df.columns.tolist()) == expected_cols
 
 
-def test_create_loaddata():
+def test_create_loaddata_ix():
     """cptools2.loaddata.create_loaddata(img_list)"""
-    output = loaddata.create_loaddata(IMG_LIST, microscope="imagexpress")
+    output = loaddata.create_loaddata(IMG_LIST_IX, microscope="imagexpress")
     # make our own
-    long_df = loaddata.create_long_loaddata(IMG_LIST, microscope="imagexpress")
+    long_df = loaddata.create_long_loaddata(IMG_LIST_IX, microscope="imagexpress")
     wide_df = loaddata.cast_dataframe(long_df)
     assert output.equals(wide_df)
+
+
+################################################################################
+# yoko
+################################################################################
+
+TEST_PATH_YOKO = os.path.join(CURRENT_PATH, "example_dir_yoko")
+TEST_PATH_PLATE_1_YOKO = os.path.join(TEST_PATH_YOKO, "test-plate-1")
+IMG_LIST_YOKO = filelist.files_from_plate(TEST_PATH_PLATE_1_YOKO)
+
+
+def test_create_long_loaddata_yoko():
+    assert False
+
+
+def test_cast_dataframe_yoko():
+    assert False
+
+
+def test_case_dataframe_yoko():
+    assert False
+
+
+################################################################################
+# opera
+################################################################################
+
+TEST_PATH_OPERA = os.path.join(CURRENT_PATH, "example_dir_opera")
+TEST_PATH_PLATE_1_OPERA = os.path.join(TEST_PATH_OPERA, "test-plate-1")
+IMG_LIST_OPERA = filelist.files_from_plate(TEST_PATH_PLATE_1_OPERA)
+
+
+def test_create_long_loaddata_opera():
+    assert False
+
+
+def test_cast_dataframe_opera():
+    assert False
+
+
+def test_case_dataframe_opera():
+    assert False
 
