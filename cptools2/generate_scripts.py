5a75f06fbcf132603476dd83c54a8dacfccc7170
Swarchal <scott.warchal@gmail.com>
2020-01-30 17:16:41 +0100

    import jdk and fix singularity typo (towards #2)

--- cptools2/generate_scripts.py
+++ cptools2/generate_scripts.py
@@ -113,8 +113,10 @@ def make_qsub_scripts(commands_location, commands_count_dict, logfile_location):
         tasks=n_tasks,
         output=os.path.join(logfile_location, "analysis")
     )
+    analysis_script += "\n"
+    analysis_script += "module load jdk"
     analysis_script += "module load singularity"
-    analysis_script += 'CP_CONTAINER="HPC_projets/CPCB-AI/singularity_containers/cellprofier_319.simg"'
+    analysis_script += 'CP_CONTAINER="HPC_projets/CPCB-AI/singularity_containers/cellprofiler_319.simg"'
     analysis_script.loop_through_file(
         cmd_path["cp_commands"],
         prefix="singularity exec $CP_CONTAINER"
